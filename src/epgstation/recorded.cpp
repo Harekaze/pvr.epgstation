ffed336282c7fb5bcb4da1eff1e12ebacd1631a4
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-07-15 03:28:14 +0900

    :vhs: Correct structure parsing of recorded API response

--- src/epgstation/recorded.cpp
+++ src/epgstation/recorded.cpp
@@ -22,35 +22,32 @@ bool Recorded::refresh()
     const bool showThumbnail = !recordedThumbnailPath.empty();
     programs.clear();
 
-    for (picojson::value& a : response.get<picojson::array>()) {
+    picojson::object& root = response.get<picojson::object>();
+
+    for (picojson::value& a : root["recorded"].get<picojson::array>()) {
         picojson::object& p = a.get<picojson::object>();
         PVR_RECORDING rec;
         char* endptr;
 
-        strncpy(rec.strRecordingId, p["id"].is<std::string>() ? p["id"].get<std::string>().c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
-        strncpy(rec.strDirectory, p["title"].is<std::string>() ? p["title"].get<std::string>().c_str() : "", PVR_ADDON_URL_STRING_LENGTH - 1);
-        strncpy(rec.strTitle, p["title"].is<std::string>() ? p["title"].get<std::string>().c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
-        strncpy(rec.strEpisodeName, p["subTitle"].is<std::string>() ? p["subTitle"].get<std::string>().c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strRecordingId, p["id"].is<double>() ? std::to_string((unsigned int)(p["id"].get<double>())).c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strDirectory, "", PVR_ADDON_URL_STRING_LENGTH - 1); // FIXME: Specify a name of series
+        strncpy(rec.strTitle, p["name"].is<std::string>() ? p["name"].get<std::string>().c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strEpisodeName, "", PVR_ADDON_NAME_STRING_LENGTH - 1); // FIXME: Specify a name of episode
         strncpy(rec.strPlotOutline, p["description"].is<std::string>() ? p["description"].get<std::string>().c_str() : rec.strEpisodeName, PVR_ADDON_DESC_STRING_LENGTH - 1);
-        strncpy(rec.strPlot, p["detail"].is<std::string>() ? p["detail"].get<std::string>().c_str() : "", PVR_ADDON_DESC_STRING_LENGTH - 1);
-        strncpy(rec.strChannelName, (p["channel"].get<picojson::object>())["name"].get<std::string>().c_str(), PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strPlot, p["extended"].is<std::string>() ? p["extended"].get<std::string>().c_str() : "", PVR_ADDON_DESC_STRING_LENGTH - 1);
+        strncpy(rec.strChannelName, "", PVR_ADDON_NAME_STRING_LENGTH - 1); // FIXME: Get channel name from channel API
         rec.iEpisodeNumber = p["episode"].is<double>() ? (unsigned int)(p["episode"].get<double>()) : 0;
-        rec.recordingTime = (time_t)(p["start"].get<double>() / 1000);
-        rec.iDuration = (int)(p["seconds"].get<double>());
-        rec.iPriority = p["priority"].is<double>() ? (int)(p["priority"].get<double>()) : 0;
-        const std::string strGenreType = p["category"].get<std::string>();
-        rec.iGenreType = epgstation::iGenreTypePair[strGenreType] & epgstation::GENRE_TYPE_MASK;
-        rec.iGenreSubType = epgstation::iGenreTypePair[strGenreType] & epgstation::GENRE_SUBTYPE_MASK;
-        std::string id = p["id"].get<std::string>();
-        std::remove(id.begin(), id.end(), '-');
-        const std::string strSubstrId = id.substr(id.size() - 6, 6);
-        rec.iEpgEventId = strtoul(strSubstrId.c_str(), &endptr, 36);
+        rec.recordingTime = (time_t)(p["startAt"].get<double>() / 1000);
+        rec.iDuration = (int)((p["endAt"].get<double>() - p["startAt"].get<double>()) / 1000);
+        rec.iPriority = 0;
+        rec.iGenreType = p["genre1"].is<double>() ? (int)(p["genre1"].get<double>()) : 0;
+        rec.iGenreSubType = p["genre2"].is<double>() ? (int)(p["genre2"].get<double>()) : 0;
+        rec.iEpgEventId = p["programId"].is<double>() ? (unsigned int)(p["programId"].get<double>()) : 0;
         rec.channelType = PVR_RECORDING_CHANNEL_TYPE_TV;
         rec.bIsDeleted = false;
-        const int sid = p["channel"].get<picojson::object>()["sid"].is<std::string>() ? std::atoi((p["channel"].get<picojson::object>()["sid"].get<std::string>()).c_str()) : (int)(p["channel"].get<picojson::object>()["sid"].get<double>());
-        rec.iChannelUid = sid;
-        if (showThumbnail) {
-            snprintf(rec.strThumbnailPath, PVR_ADDON_URL_STRING_LENGTH - 1, (const char*)(epgstation::api::baseURL + recordedThumbnailPath).c_str(), p["id"].get<std::string>().c_str());
+        rec.iChannelUid = p["channelId"].is<double>() ? (int)(p["channelId"].get<double>()) : 0;
+        if (showThumbnail && p["hasThumbnail"].is<bool>() && p["hasThumbnail"].get<bool>()) {
+            snprintf(rec.strThumbnailPath, PVR_ADDON_URL_STRING_LENGTH - 1, (const char*)(epgstation::api::baseURL + recordedThumbnailPath).c_str(), rec.strRecordingId);
         } else {
             strncpy(rec.strThumbnailPath, "", PVR_ADDON_URL_STRING_LENGTH - 1);
         }
