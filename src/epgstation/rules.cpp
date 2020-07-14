47ece6b4ad5664292dfa2efc535fa54e191bfb5b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-07-15 03:56:27 +0900

    :copyright: Modernize copyright header

--- src/epgstation/rules.cpp
+++ src/epgstation/rules.cpp
@@ -1,23 +1,7 @@
 /*
- *         Copyright (C) 2015-2018 Yuki MIZUNO
- *         https://github.com/Harekaze/pvr.epgstation/
- *
- *
- * This file is part of pvr.epgstation.
- *
- * pvr.epgstation is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * pvr.epgstation is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with pvr.epgstation.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (C) 2015-2020 Yuki MIZUNO
+ * https://github.com/Harekaze/pvr.epgstation/
+ * SPDX-License-Identifier: GPL-3.0-only
  */
 #include "api.h"
 #include "reserves.h"
