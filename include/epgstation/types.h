05bd5dda62ef961f675f72b822979217d5c3addc
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-07-18 02:49:54 +0900

    :cool: Add recorded API response mapper

--- include/epgstation/types.h
+++ include/epgstation/types.h
@@ -16,6 +16,44 @@ public:
 
     NLOHMANN_DEFINE_TYPE_INTRUSIVE(storage, total, used);
 };
+
+class recorded {
+public:
+    unsigned int id;
+    unsigned long programId;
+    unsigned long channelId;
+    std::string channelType;
+    std::string name;
+    std::string description;
+    std::string extended;
+    time_t startAt;
+    time_t endAt;
+    unsigned int genre1;
+    unsigned int genre2;
+    bool recording;
+    bool hasThumbnail;
+
+    friend void from_json(const nlohmann::json& j, recorded& r)
+    {
+        j.at("id").get_to(r.id);
+        j.at("programId").get_to(r.programId);
+        j.at("channelId").get_to(r.channelId);
+        j.at("channelType").get_to(r.channelType);
+        j.at("name").get_to(r.name);
+        if (j.contains("description")) {
+            j.at("description").get_to(r.description);
+        }
+        if (j.contains("extended")) {
+            j.at("extended").get_to(r.extended);
+        }
+        r.startAt = j["startAt"].get<unsigned long>() / 1000;
+        r.endAt = j["endAt"].get<unsigned long>() / 1000;
+        j.at("genre1").get_to(r.genre1);
+        j.at("genre2").get_to(r.genre2);
+        j.at("recording").get_to(r.recording);
+        j.at("hasThumbnail").get_to(r.hasThumbnail);
+    }
+};
 } // namespace epgstation
 
 #endif /* end of include guard */
--- src/epgstation/recorded.cpp
+++ src/epgstation/recorded.cpp
@@ -5,6 +5,7 @@
  */
 #include "recorded.h"
 #include "api.h"
+#include "epgstation/types.h"
 #include "kodi/libXBMC_addon.h"
 #include "schedule.h"
 #include "json/json.hpp"
@@ -26,29 +27,32 @@ bool Recorded::refresh()
     for (nlohmann::json& p : response["recorded"]) {
         PVR_RECORDING rec;
         char* endptr;
+        epgstation::recorded r = p.get<epgstation::recorded>();
 
-        strncpy(rec.strRecordingId, p["id"].is_number() ? std::to_string((unsigned int)(p["id"].get<double>())).c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
-        strncpy(rec.strDirectory, "", PVR_ADDON_URL_STRING_LENGTH - 1); // FIXME: Specify a name of series
-        strncpy(rec.strTitle, p["name"].is_string() ? p["name"].get<std::string>().c_str() : "", PVR_ADDON_NAME_STRING_LENGTH - 1);
-        strncpy(rec.strEpisodeName, "", PVR_ADDON_NAME_STRING_LENGTH - 1); // FIXME: Specify a name of episode
-        strncpy(rec.strPlotOutline, p["description"].is_string() ? p["description"].get<std::string>().c_str() : rec.strEpisodeName, PVR_ADDON_DESC_STRING_LENGTH - 1);
-        strncpy(rec.strPlot, p["extended"].is_string() ? p["extended"].get<std::string>().c_str() : "", PVR_ADDON_DESC_STRING_LENGTH - 1);
-        strncpy(rec.strChannelName, "", PVR_ADDON_NAME_STRING_LENGTH - 1); // FIXME: Get channel name from channel API
-        rec.iEpisodeNumber = p["episode"].is_number() ? (unsigned int)(p["episode"].get<double>()) : 0;
-        rec.recordingTime = (time_t)(p["startAt"].get<double>() / 1000);
-        rec.iDuration = (int)((p["endAt"].get<double>() - p["startAt"].get<double>()) / 1000);
-        rec.iPriority = 0;
-        rec.iGenreType = p["genre1"].is_number() ? (int)(p["genre1"].get<double>()) : 0;
-        rec.iGenreSubType = p["genre2"].is_number() ? (int)(p["genre2"].get<double>()) : 0;
-        rec.iEpgEventId = p["programId"].is_number() ? (unsigned int)(p["programId"].get<double>()) : 0;
+        strncpy(rec.strRecordingId, std::to_string(r.id).c_str(), PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strTitle, r.name.c_str(), PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strPlotOutline, r.description.c_str(), PVR_ADDON_DESC_STRING_LENGTH - 1);
+        strncpy(rec.strPlot, r.extended.c_str(), PVR_ADDON_DESC_STRING_LENGTH - 1);
+        rec.recordingTime = r.startAt;
+        rec.iDuration = (int)(r.endAt - r.startAt);
+        rec.iGenreType = r.genre1;
+        rec.iGenreSubType = r.genre2;
+        rec.iEpgEventId = r.programId;
+        rec.iChannelUid = r.channelId;
         rec.channelType = PVR_RECORDING_CHANNEL_TYPE_TV;
-        rec.bIsDeleted = false;
-        rec.iChannelUid = p["channelId"].is_number() ? (int)(p["channelId"].get<double>()) : 0;
-        if (showThumbnail && p["hasThumbnail"].is_boolean() && p["hasThumbnail"].get<bool>()) {
+        if (showThumbnail && r.hasThumbnail) {
             snprintf(rec.strThumbnailPath, PVR_ADDON_URL_STRING_LENGTH - 1, (const char*)(epgstation::api::baseURL + recordedThumbnailPath).c_str(), rec.strRecordingId);
         } else {
             strncpy(rec.strThumbnailPath, "", PVR_ADDON_URL_STRING_LENGTH - 1);
         }
+
+        // Not available in API response
+        strncpy(rec.strDirectory, "", PVR_ADDON_URL_STRING_LENGTH - 1);
+        strncpy(rec.strEpisodeName, "", PVR_ADDON_NAME_STRING_LENGTH - 1);
+        strncpy(rec.strChannelName, "", PVR_ADDON_NAME_STRING_LENGTH - 1);
+        rec.iEpisodeNumber = 0;
+        rec.iPriority = 0;
+        rec.bIsDeleted = false;
 
         programs.push_back(rec);
     }
