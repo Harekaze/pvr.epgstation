e0d60490ff386618e09bcbde065f1f1232c157a9
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-07-22 23:36:04 +0900

    :wrench: Fix Visual Studio build files

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 & ${env:ProgramFiles(x86)}\Microsoft` Visual` Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe .\VS2019\pvr.epgstation.sln /t:Clean,Build /p:Configuration=Release /p:Platform=x86
 New-Item dist -ItemType Directory
 Copy-Item -r .\template\pvr.epgstation .\dist\
@@ -6,5 +7,3 @@ Copy-Item .\LICENSE .\dist\pvr.epgstation
 Copy-Item .\VS2019\Release\pvr.epgstation.dll .\dist\pvr.epgstation
 Set-Location .\dist
 Compress-Archive -Force -CompressionLevel NoCompression -Path .\pvr.epgstation -DestinationPath ..\pvr.epgstation.zip
-Write-Warning "PowerShell command 'Compress-Archive' creates broken zip file.
-Please unzip .\pvr.epgstation.zip yourself, and re-zip it with other compression tool."
