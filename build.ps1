c3fce51eaa7d0d0701b6ce80c4597e22dc671b4d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-07-27 08:03:57 +0900

    :wrench: Add x64 build parameter option

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
+Param([ValidateSet("x86","x64")][String] $Platform = "x86")
 $ErrorActionPreference = "Stop"
-& ${env:ProgramFiles(x86)}\Microsoft` Visual` Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe .\VS2019\pvr.epgstation.sln /t:Clean,Build /p:Configuration=Release /p:Platform=x86
-New-Item dist -ItemType Directory
+& ${env:ProgramFiles(x86)}\Microsoft` Visual` Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe .\VS2019\pvr.epgstation.sln /t:Clean,Build /p:Configuration=Release /p:Platform=$Platform
+New-Item dist -Force -ItemType Directory
 Copy-Item -r .\template\pvr.epgstation .\dist\
 Copy-Item .\ChangeLog.txt .\dist\pvr.epgstation
 Copy-Item .\LICENSE .\dist\pvr.epgstation
-Copy-Item .\VS2019\Release\pvr.epgstation.dll .\dist\pvr.epgstation
+Copy-Item .\VS2019\Release\pvr.epgstation*.dll .\dist\pvr.epgstation
 Set-Location .\dist
 Compress-Archive -Force -CompressionLevel NoCompression -Path .\pvr.epgstation -DestinationPath ..\pvr.epgstation.zip
